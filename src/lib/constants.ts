df163a3e2647abd8860173966c281b3a103f9bb1
Dammy Ola <dammyolapaix@gmail.com>
2025-03-27 17:12:13 +0000

    Add more constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,9 @@
 export const OAUTH_PROVIDERS = ['google'] as const
 
 export const USER_ROLES = ['buyer', 'seller', 'admin'] as const
+
+export const UNAUTHENTICATED_ERROR_MESSAGE =
+  'You must be logged in to perform this action' as const
+
+export const INTERNAL_ERROR_MESSAGE =
+  'Something went wrong from our end, please try again later. Report the issue if it persists' as const
